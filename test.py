cf96608ce6c95610092459fbf9e0a62cf3ff1ee3
unknown <lfrackowiak4@gmail.com>
2022-03-13 14:41:11 +0100

    dodano kalkulator z dodawaniem

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
+
+
 def hello(name):
 return "Hello" + str(name)
+def dodaj(a,b):
+      wynik = float(a) + float(b)
+      return wynik
+
+pierwsza = input()
+druga = input()
+
+print (dodaj(pierwsza, druga))
+
 
 print ("Hello world")
